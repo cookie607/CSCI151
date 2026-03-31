2a3613abccd53e8d9a9327452d7fe3f395e2d9ee
Preston <prestonfields734@gmail.com>
2026-03-30 20:55:57 -0600

    working game

--- game.py
+++ game.py
@@ -6,7 +6,9 @@ from gamefunctions import (
     print_welcome,
     print_shop_menu,
     purchase_item,
-    random_monster
+    random_monster,
+    fight,
+    rest
     )
 
 def main():
@@ -17,22 +19,40 @@ def main():
     print(print_welcome(name, 50))
     print()
 
-    # Show shop
-    print("Welcome to the shop!")
-    print_shop_menu("potion", 5, "sword", 20)
-    print()
+    player_info = {
+        "health": 20,
+        "power": 4,
+        "money": 30
+    }
 
-    # Purchase item
-    print("Let's buy something:")
-    purchase_item()
-    print()
+    quest = True
 
-    # Encounter monster
-    print("Time for a battle!")
-    monster = random_monster()
+    while quest:
+        choice = input(                                # player game options
+            " \n1) Adventure into the woods."
+            " \n2) Rest."
+            " \n3) Abandon run.\n"
+            "Input: [choose 1,2 or 3]"
+            ).lower().strip()
 
-    print("\nGood luck,", name + "!")
+        if choice == "1":
+            my_monster = random_monster()              # create monster
+            player_info = fight(player_info, my_monster)  # fight it
 
+            if player_info["health"] <= 0:             # check player has health
+                print("Game Over!")                         #end the game
+                quest = False
 
-if __name__ == "__main__":
-    main()
+        elif choice == "2":                         #initiates the rest
+            
+            rest(player_info)
+        
+        elif choice == "3":                         #quits the game
+            print(f"You did well {name}!")
+            print("You have abandoned the run.")
+            quest = False
+
+        else:                                       #the input is invalid prompts new input
+            print("Invalid input, please enter an accepted input")
+     
+main()
